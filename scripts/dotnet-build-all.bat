477f4f4f013376b385b587cea08762c5a999fe90
s3rfin <serfin009@gmail.com>
2019-10-05 23:25:34 +0200

    Added identity service to build plan

--- scripts/dotnet-build-all.bat
+++ scripts/dotnet-build-all.bat
@@ -3,8 +3,12 @@
 REM Build Common lib first
 set path[0]="BeComfy.Common/src/BeComfy.Common"
 
+REM Build API
 set path[1]="BeComfy.Api/src/BeComfy.Api"
+
+REM Build Microservices
 set path[2]="BeComfy.Services.Flights/src/BeComfy.Services.Flights"
+set path[3]="BeComfy.Services.Identity/src/BeComfy.Services.Identity"
 
 set "x=0"
 cd ../..

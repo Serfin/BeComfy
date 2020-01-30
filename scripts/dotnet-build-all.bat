e935757f7bbb438ba15212e1078188b4299ffae3
serfin <serfin009@gmail.com>
2020-01-30 23:44:29 +0100

    Changed build plan for new microservice

--- scripts/dotnet-build-all.bat
+++ scripts/dotnet-build-all.bat
@@ -1,27 +1,43 @@
 @echo off
 
 REM Build Common lib first
-set path[0]="BeComfy.Common/src/BeComfy.Common"
+set list="BeComfy.Common/src/BeComfy.Common"
 
 REM Build API
-set path[1]="BeComfy.Api/src/BeComfy.Api"
+set list=%list%;"BeComfy.Api/src/BeComfy.Api"
 
 REM Build Microservices
-set path[2]="BeComfy.Services.Flights/src/BeComfy.Services.Flights"
-set path[3]="BeComfy.Services.Airplanes/src/BeComfy.Services.Airplanes"
-set path[4]="BeComfy.Services.Identity/src/BeComfy.Services.Identity"
-set path[5]="BeComfy.Services.Tickets/src/BeComfy.Services.Tickets"
+set list=%list%;"BeComfy.Services.Flights/src/BeComfy.Services.Flights"
+set list=%list%;"BeComfy.Services.Airplanes/src/BeComfy.Services.Airplanes"
+set list=%list%;"BeComfy.Services.Identity/src/BeComfy.Services.Identity"
+set list=%list%;"BeComfy.Services.Tickets/src/BeComfy.Services.Tickets"
+set list=%list%;"BeComfy.Services.Customers/src/BeComfy.Services.Customers.Api"
 
-set "x=0"
 cd ../..
 
-:SymLoop
-if defined path[%x%] (
-    call cd %%path[%x%]%%
+@echo off
+
+echo ======================================================================
+echo                       BeComfy - Build plan
+echo ====================================================================== 
+
+
+for %%a in (%list%) do (
+    echo ======================================================================
+    echo    Build %%a
+    echo ======================================================================
+    echo/
+
+    call cd %%a
     call dotnet build
+
+    echo/
+    echo ======================================================================
+    echo    Done %%a
+    echo ======================================================================
+    echo/
+
     call cd ../../..
-    set /a "x+=1"
-    GOTO :SymLoop
 )
 
 cd "BeComfy/scripts"

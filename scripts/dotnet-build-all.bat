9da3bda4eda59201f001268f8723656c4f3805d3
serfin <serfin009@gmail.com>
2020-04-27 22:20:41 +0200

    Repo cleanup

--- scripts/dotnet-build-all.bat
+++ scripts/dotnet-build-all.bat
@@ -2,7 +2,7 @@
 
 REM Build Common lib first
 set list="BeComfy.Common/src/BeComfy.Common"
-set list="BeComfy.Common/src/BeComfy.Logging.Elk"
+set list="BeComfy.Common/src/BeComfy.Logging"
 
 REM Build API
 set list=%list%;"BeComfy.Api/src/BeComfy.Api"
